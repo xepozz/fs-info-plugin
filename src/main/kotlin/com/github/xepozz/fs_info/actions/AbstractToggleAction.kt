fa7424a7651722918fd169688b720c33312388ef
Dmitriy Derepko <xepozz@list.ru>
2025-08-26 14:31:28 +0400

    refactor: introduce toggle factory

---
+++ src/main/kotlin/com/github/xepozz/fs_info/actions/AbstractToggleAction.kt
@@ -0,0 +1,33 @@
+package com.github.xepozz.fs_info.actions
+
+import com.github.xepozz.fs_info.FsInfoSettings
+import com.intellij.ide.projectView.ProjectView
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.ToggleOptionAction
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import kotlin.reflect.KMutableProperty0
+
+abstract class AbstractToggleAction(action: AnActionEvent) : ToggleOptionAction.Option {
+    val project: Project = action.project!!
+    val settings = project.service<FsInfoSettings>()
+
+    override fun isSelected() = option.get()
+
+    override fun setSelected(selected: Boolean) {
+        val updated = selected != isSelected
+
+        option.set(selected)
+
+        if (updated) {
+            project.let { project ->
+                ProjectView
+                    .getInstance(project)
+                    .currentProjectViewPane
+                    ?.updateFromRoot(true)
+            }
+        }
+    }
+
+    abstract val option: KMutableProperty0<Boolean>
+}
---
+++ src/main/kotlin/com/github/xepozz/fs_info/actions/actions.kt
@@ -0,0 +1,40 @@
+package com.github.xepozz.fs_info.actions
+
+import com.intellij.openapi.actionSystem.ToggleOptionAction
+import com.intellij.openapi.project.DumbAware
+
+class ToggleBytesAction : DumbAware, ToggleOptionAction({
+    object : AbstractToggleAction(it) {
+        override val option = settings::showBytes
+    }
+})
+
+class ToggleKBytesAction : DumbAware, ToggleOptionAction({
+    object : AbstractToggleAction(it) {
+        override val option = settings::showKBytes
+    }
+})
+
+class ToggleMBytesAction : DumbAware, ToggleOptionAction({
+    object : AbstractToggleAction(it) {
+        override val option = settings::showMBytes
+    }
+})
+
+class ToggleGBytesAction : DumbAware, ToggleOptionAction({
+    object : AbstractToggleAction(it) {
+        override val option = settings::showGBytes
+    }
+})
+
+class ToggleLinesAction : DumbAware, ToggleOptionAction({
+    object : AbstractToggleAction(it) {
+        override val option = settings::showLines
+    }
+})
+
+class ToggleDirectoryItemsAction : DumbAware, ToggleOptionAction({
+    object : AbstractToggleAction(it) {
+        override val option = settings::showDirectoryItems
+    }
+})
