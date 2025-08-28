a898ae9405a23f886de25dbeb36eb62315b555ec
Dmitriy Derepko <xepozz@list.ru>
2025-08-28 19:52:51 +0400

    fix: compatibility problems

--- src/main/kotlin/com/github/xepozz/fs_info/FileStatusListener.kt
+++ src/main/kotlin/com/github/xepozz/fs_info/FileStatusListener.kt
@@ -2,10 +2,9 @@ package com.github.xepozz.fs_info
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vcs.FileStatusListener
-import com.intellij.util.ui.update.MergingUpdateQueue
-import com.intellij.util.ui.update.Update
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class FileStatusListener(
@@ -13,20 +12,12 @@ class FileStatusListener(
     private val coroutineScope: CoroutineScope,
 ) : FileStatusListener {
     val fileSystemService: FileSystemService = project.getService(FileSystemService::class.java)
-
-    val queue = MergingUpdateQueue("FileSystemCollector.Queue", 200, true, null)
-    val update = object : Update(this) {
-        override fun run() {
-            coroutineScope.launch(Dispatchers.IO) {
-                fileSystemService.refresh()
-            }
-        }
-
-        override fun canEat(update: Update) = true
-    }
+    var job: Job? = null
 
     override fun fileStatusesChanged() {
-        queue.cancelAllUpdates()
-        queue.queue(update)
+        job?.cancel()
+        job = coroutineScope.launch(Dispatchers.IO) {
+            fileSystemService.refresh()
+        }
     }
 }
